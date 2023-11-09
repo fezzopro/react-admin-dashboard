48ce28abf29d07fca0f1762a763266390fde583d
Fezzopro <fezzopro@gmail.com>
2023-11-09 22:33:40 +0200

    Reduce topbar code complexity

--- src/scenes/global/Topbar/Topbar.jsx
+++ src/scenes/global/Topbar/Topbar.jsx
@@ -1,14 +1,10 @@
 import {
-  InputBase, Box, IconButton, useTheme,
+  Box, useTheme,
 } from '@mui/material';
 import { useContext } from 'react';
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationAddOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutline';
-import SearchIcon from '@mui/icons-material/Search';
 import { ColorModeContext, tokens } from '../../../theme';
+import SearchBar from './SearchBar';
+import TopBarIcons from './TopBarIcons';
 import './topbar.scss';
 
 const Topbar = () => {
@@ -19,32 +15,10 @@ const Topbar = () => {
   return (
     <Box className="top-bar">
       {/* SEARCH BAR */}
-      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-        <IconButton type="button" sx={{ p: 1 }}>
-          <SearchIcon />
-        </IconButton>
-      </Box>
+      <SearchBar colors={colors} />
 
       {/* ICONS */}
-      <Box display="flex">
-        <IconButton onClick={toggleColorMode}>
-          {theme.palette.mode === 'dark' ? (
-            <DarkModeOutlinedIcon />
-          ) : (
-            <LightModeOutlinedIcon />
-          )}
-        </IconButton>
-        <IconButton>
-          <NotificationsOutlinedIcon />
-        </IconButton>
-        <IconButton>
-          <SettingsOutlinedIcon />
-        </IconButton>
-        <IconButton>
-          <PersonOutlinedIcon />
-        </IconButton>
-      </Box>
+      <TopBarIcons toggleColorMode={toggleColorMode} theme={theme} />
     </Box>
   );
 };
