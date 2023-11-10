12963490a665c58c72529b9b61b1743e9e40813c
Fezzopro <fezzopro@gmail.com>
2023-11-10 18:13:53 +0200

    Activate invoices route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Topbar from './scenes/global/Topbar/Topbar';
 import Sidebar from './scenes/global/Sidebar/Sidebar';
 import Dashboard from './scenes/Dashboard';
 import Team from './scenes/Team';
-// import Invoices from './scenes/Invoices';
+import Invoices from './scenes/Invoices';
 import Contacts from './scenes/Contacts';
 // import Bar from './scenes/Bar';
 // import Form from './scenes/Form';
@@ -29,8 +29,8 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
