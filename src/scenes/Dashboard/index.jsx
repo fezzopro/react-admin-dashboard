d72231dbe242439d015fc4b12741d87b4514c835
Fezzopro <fezzopro@gmail.com>
2023-11-10 13:03:22 +0200

    Add Header component to the dashboard

--- src/scenes/Dashboard/index.jsx
+++ src/scenes/Dashboard/index.jsx
@@ -1,3 +1,15 @@
-const Dashboard = () => (<div>Dashboard</div>);
+import { Box } from '@mui/material';
+import Header from '../../components/Shared/Header/Header';
+
+const Dashboard = () => (
+  <Box m="20px">
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Header
+        title="DASHBOARD"
+        subTitle="Welcome to your dashboard"
+      />
+    </Box>
+  </Box>
+);
 
 export default Dashboard;
