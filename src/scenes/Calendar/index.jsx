871d47b95a07f659444b04a0310d28bc2713589c
Fezzopro <fezzopro@gmail.com>
2023-11-16 21:30:55 +0200

    Add calendar scene

---
+++ src/scenes/Calendar/index.jsx
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import FullCalendar, { formatDate } from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import Header from '../../components/Shared/Header/Header';
+import { tokens } from '../../theme';
+
+const Calendar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvent, setCurrentEvent] = useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt('Please Enter a new title for your event');
+    const calendarAPI = selected.view.calendar;
+    calendarAPI.unselect();
+
+    if (title) {
+      calendarAPI.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
+  };
+
+  const handleEventClick = (selected) => {
+    if (window.confirm(`Are you sure you want to delete this event '${selected.event.title}'`)) {
+      selected.event.remove();
+    }
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="CALENDAR" subTitle="Full Interactive Calendar" />
+      <Box display="flex" justifyContent="space-between">
+        {/* CALENDAR SIDEBAR */}
+        <Box flex="1 1 20%" backgroundColor={colors.primary[400]} p="15px" borderRadius="4px">
+          <Typography variant="h5">Event</Typography>
+          <List>
+            {currentEvent.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: '10px 0',
+                  borderRadius: '2px',
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: 'numeric',
+                        month: 'short',
+                        day: 'numeric',
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        {/* CALENDAR */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: 'prev,next,today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth',
+            }}
+            initialView="dayGridMonth"
+            editable
+            selectable
+            selectMirror
+            dayMaxEvents
+            select={handleDateClick}
+            handleEventClick={handleEventClick}
+            eventsSet={(event) => setCurrentEvent(event)}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Calendar;
