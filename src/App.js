0b446e132b3115c73e881c46a0b0668cf1088c3d
Fezzopro <fezzopro@gmail.com>
2023-11-23 16:19:27 +0200

    Allow Pie Chart route to application

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Contacts from './scenes/Contacts';
 import Bar from './scenes/Bar';
 import Form from './scenes/Form';
 // import Line from './scenes/Line';
-// import Pie from './scenes/Pie';
+import Pie from './scenes/Pie';
 import FAQ from './scenes/FAQ';
 // import Geograph from './scenes/Geograph';
 import Calendar from './scenes/Calendar';
@@ -33,8 +33,8 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
-              {/* <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
+              <Route path="/pie" element={<Pie />} />
+              {/* <Route path="/line" element={<Line />} />
             <Route path="/geograph" element={<Geograph />} /> */}
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
