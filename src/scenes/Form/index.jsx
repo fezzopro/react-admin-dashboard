2da0887914555f64957ec389b1ed731a74b25826
Felix KAMANA <42069952+fezzopro@users.noreply.github.com>
2026-07-14 19:10:00 +0200

    Disable no-unused-vars errors

--- src/scenes/Form/index.jsx
+++ src/scenes/Form/index.jsx
@@ -26,7 +26,7 @@ const userSchema = yup.object().shape({
 
 const Form = () => {
   const isNonMobile = useMediaQuery('(min-width: 600px)');
-
+  /*eslint no-unused-vars: "error"*/
   const handleFormSubmit = (values) => {
     // console.log(values);
   };
